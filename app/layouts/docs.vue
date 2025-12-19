122331b629fa0b1ade6556090700b83d55b87cb6
bal16 <ahmadhamidbalya@gmail.com>
2025-12-19 17:37:58 +0700

    refactor: improve layout structure by adding Header and Footer components

--- app/layouts/docs.vue
+++ app/layouts/docs.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
+import Header from "~/components/Header.vue";
 import Sidebar from "~/components/Sidebar.vue";
+import Footer from "~/components/Footer.vue";
 </script>
 
 <template>
   <NuxtLayout name="default">
+    <!-- Main Layout: Sidebar (Fixed) + Content (Scrollable) -->
     <div class="flex flex-1 overflow-hidden h-[calc(100vh-3.5rem)]">
       <Sidebar />
-
       <main class="flex-1 overflow-y-auto">
-        <div class="container max-w-5xl py-6 lg:py-8">
+        <div class="container max-w-5xl py-6 lg:py-8 mx-auto">
           <slot />
         </div>
       </main>
