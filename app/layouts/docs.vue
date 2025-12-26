78f069d7fd17e4d5cfe03f812abe57b026f04526
bal16 <ahmadhamidbalya@gmail.com>
2025-12-26 14:18:10 +0700

    style: remove unused imports

--- app/layouts/docs.vue
+++ app/layouts/docs.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import Header from "~/components/Header.vue";
 import Sidebar from "~/components/Sidebar.vue";
-import Footer from "~/components/Footer.vue";
+import MobileHeader from "~/components/MobileHeader.vue";
 </script>
 
 <template>
   <NuxtLayout name="default">
+    <MobileHeader />
     <!-- Main Layout: Sidebar (Fixed) + Content (Scrollable) -->
     <div class="flex flex-1 overflow-hidden h-[calc(100vh-3.5rem)]">
       <Sidebar />
