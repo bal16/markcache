fce8421dd5a66cad89ca0c6424053050415aabb7
bal16 <ahmadhamidbalya@gmail.com>
2025-12-27 10:46:51 +0700

    refactor: separate to new component

---
+++ app/components/GoToTop.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { ArrowUp } from "lucide-vue-next";
+import { Button } from "~/components/ui/button";
+
+function scrollToTop() {
+  const topElement = document.getElementById("page-top");
+  if (topElement) {
+    topElement.scrollIntoView({ behavior: "smooth" });
+  }
+}
+</script>
+<template>
+  <Button
+    @click.prevent="scrollToTop"
+    variant="ghost"
+    class="inline-flex items-center text-sm font-medium text-muted-foreground hover:text-foreground"
+  >
+    Back To Top <ArrowUp class="ml-2 h-4 w-4" />
+  </Button>
+</template>
