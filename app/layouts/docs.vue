f13d68e8ed933e0a22e4566c7a6bf4ac9e63bdd1
bal16 <ahmadhamidbalya@gmail.com>
2026-08-07 21:46:36 +0700

    fix: resolve layout overflow and cut-off in docs layout

--- app/layouts/docs.vue
+++ app/layouts/docs.vue
@@ -5,15 +5,17 @@ import Sidebar from '~/components/Sidebar.vue'
 
 <template>
   <NuxtLayout name="default">
-    <MobileHeader />
-    <!-- Main Layout: Sidebar (Fixed) + Content (Scrollable) -->
-    <div class="flex flex-1 overflow-hidden h-[calc(100vh-3.5rem)]">
-      <Sidebar />
-      <main class="flex-1 overflow-y-auto">
-        <div class="container max-w-5xl py-6 lg:py-8 mx-auto">
-          <slot />
-        </div>
-      </main>
+    <div class="flex flex-col h-full w-full min-h-0">
+      <MobileHeader />
+      <!-- Main Layout: Sidebar (Fixed) + Content (Scrollable) -->
+      <div class="flex flex-1 overflow-hidden">
+        <Sidebar />
+        <main class="flex-1 overflow-y-auto">
+          <div class="container max-w-5xl py-6 lg:py-8 mx-auto">
+            <slot />
+          </div>
+        </main>
+      </div>
     </div>
   </NuxtLayout>
 </template>
